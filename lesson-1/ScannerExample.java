629cf5bf8b42b8938af86467b97fdd62ea3b4456
danidev94 <daniel.lp1094@gmail.com>
2024-03-19 11:04:34 +0100

    feat: add ScannerExample.java

---
+++ lesson-1/ScannerExample.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class ScannerExample {
+    public static void main(String[] args) {
+        Scanner keyboardInput = new Scanner(System.in);
+        // basicUsage(keyboardInput);
+        basicError(keyboardInput);
+    }
+
+    public static void basicUsage(Scanner scanner) {
+        System.out.print("What's your name?: ");
+        String enteredText = scanner.next(); // nextLine() para leer toda la línea hasta llegar a un espacio en blanco
+        // String enteredText = entryKeyBoard.nextLine(); // nextLine() para leer toda
+        // la línea hasta encontrar un salto de línea
+        System.out.println("Hello " + enteredText);
+        scanner.close();
+    }
+
+    public static void basicError(Scanner scanner) {
+        System.out.print("Enter your age: ");
+        int number = scanner.nextInt();
+        // scanner.nextLine(); // Limpiar el buffer si no se realiza salta el error.(Comentar y descomentar para ver el error)
+        System.out.println("Your age is: " + number);
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+
+        if (name.isEmpty())
+            System.out.println("Ups! We've triggered the error, it seems like it's not working!");
+        else
+            System.out.println("Your name is: " + name); // Nunca se ejecuta ya que al utilizar nextInt() se queda un salto de línea en el buffer (\n)
+
+        scanner.close();
+    }
+}
